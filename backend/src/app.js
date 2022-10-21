8356c0ca4c55f258701c04c0b0573c1eeefe1f3b
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2022-10-21 15:51:25 -0300

    Adicionado router.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => response.status(200).send('Olá, Mundo!'));
+/*app.get('/', (request, response) => response.status(200).send('Olá, Mundo!'));*/
 
 module.exports = app;
